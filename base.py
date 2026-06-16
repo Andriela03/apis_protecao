4915c6399ce185831f00db7a7baf2b81aaa895c2
Andriela Gleice Pereira da Silva <andriela.silva@escolar.ifrn.edu.br>
2026-06-16 20:49:49 -0300

    Parte 1 finalizada

--- base.py
+++ base.py
@@ -1,8 +1,18 @@
 from flask import Flask, jsonify
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+USUARIO = "admin"
+SENHA = "123"
+
+@auth.verify_password
+def verify_password(username, password):
+    return username == USUARIO and password == SENHA
 
 @app.route("/hello", methods=["GET"])
+@auth.login_required
 def hello():
     return jsonify({
         "mensagem": "Hello World"
