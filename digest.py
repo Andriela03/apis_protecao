e4a952758a1337fae6d086be518d1cf7f5b6031b
Acsa Mirella <mirella.a@escolar.ifrn.edu.br>
2026-06-16 21:43:49 -0300

    feat: Adicionar digest / fix: basic

--- digest.py
+++ digest.py
@@ -1,12 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_httpauth import HTTPDigestAuth
 
 app = Flask(__name__)
+app.secret_key = 'chave-secreta-digest'
 
-@app.route("/hello", methods=["GET"])
+auth = HTTPDigestAuth(realm="hello")
+
+USERS = {
+    "admin": "123"
+}
+
+@auth.get_password
+def get_password(username):
+    return USERS.get(username)
+
+@app.route('/hello', methods = ["GET"])
+@auth.login_required
 def hello():
-    return jsonify({
-        "mensagem": "Hello World"
-    })
+    return f'Olá, {auth.current_user()}!'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
